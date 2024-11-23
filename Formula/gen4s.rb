f9437d5ef2edb7b90f90dde5ebcc5bb74d5a6b61
Denys Kovalevskyi <xdev.developer@gmail.com>
2024-11-23 22:54:07 +0100

    Gen4s v0.0.20

--- Formula/gen4s.rb
+++ Formula/gen4s.rb
@@ -1,9 +1,9 @@
 class Gen4s < Formula
   desc "Data generator tool for developers and QA engineers"
   homepage "https://github.com/xdev-developer/gen4s"
-  url "https://github.com/xdev-developer/gen4s/releases/download/release-v0.0.19/gen4s-app-0.0.19.zip"
-  version "0.0.19"
-  sha256 "e1bf4cfb687a8a2c5b077f5222ff9450512f8e298d6bc77736b4dfc4994f5881"
+  url "https://github.com/xdev-developer/gen4s/releases/download/release-v0.0.20/gen4s-app-0.0.20.zip"
+  version "0.0.20"
+  sha256 "e2ef070c560f3ab53f828f84f13a2050af0787a566b67e6e2ae199c33623fcde"
 
   depends_on "openjdk"
 
--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # homebrew-tap
 
+## Instalation
+
 ```shell
 brew tap xdev-developer/tap
 brew install gen4s
+```
+
+## Building from source
+```shell
+HOMEBREW_NO_INSTALL_FROM_API=1 brew reinstall --build-from-source --verbose --debug ./Formula/gen4s.rb
 ```
