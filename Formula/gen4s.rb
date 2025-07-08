525512964ec0f0d3a2cf0dbaee6fb562c0d77188
Denys Kovalevskyi <xdev.developer@gmail.com>
2025-07-08 18:34:18 +0200

    Release 0.1.1

--- Formula/gen4s.rb
+++ Formula/gen4s.rb
@@ -1,9 +1,9 @@
 class Gen4s < Formula
   desc "Data generator tool for developers and QA engineers"
   homepage "https://github.com/xdev-developer/gen4s"
-  url "https://github.com/xdev-developer/gen4s/releases/download/release-v0.1.0/gen4s-app-0.1.0.zip"
-  version "0.1.0"
-  sha256 "5041578805455473ce9ac79d64d835823b42088d2aa21979d6359e17083fa2c1"
+  url "https://github.com/xdev-developer/gen4s/releases/download/release-v0.1.1/gen4s-app-0.1.1.zip"
+  version "0.1.1"
+  sha256 "8c1ebed6619020d22726bee2ae1e08b2817b37d009b410129aa5a8ed9058355e"
 
   depends_on "openjdk"
 
